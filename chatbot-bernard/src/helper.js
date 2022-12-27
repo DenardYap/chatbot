be909ac562776202742ea2373aa3c0cb9be66392
bernard <mameehy@hotmail.com>
2022-12-26 20:10:25 -0500

    added number encoder

--- chatbot-bernard/src/helper.js
+++ chatbot-bernard/src/helper.js
@@ -7,10 +7,13 @@ import  * as tf from "@tensorflow/tfjs"
 import training_data from './data.json';
 
 const wordSet = new Set(
-    ['{', '`', '"', ']', '-', '/', ':', '!', ')', '@', '\\', '*', '~', '#', '(', '%', '_', '.', '^', ',', '>', '}', '[', '=', '+', '&', '?', '|', '<', '$', ';']
+    ['{', '`', '"', ']', '-', '/', ':', '!', ')', '@', '\\', '*', '~', '#', '(', '%', '_', '.', '^', ',', '>', '}', '[', '=', '&', '?', '|', '<', '$', ';']
 );
 
 function isNumeric(s) {
+    if (s == "1") {
+        return false
+    }
     return !isNaN(s - parseFloat(s));
 }
 
@@ -28,6 +31,14 @@ function extract(input_string){
     return newS.split(" ")
 }
 
+// in preprocessing we stripped off all the numbers and 
+// special characters, but the speech recognition API 
+// will convert things such as 'one plus one' as '1 + 1'
+// thus, we need to encode '1 + 1' back to 'one plus one' here
+const SPECIAL_CHARACTER_MAP = {
+    "1" : "one",
+    "+" : "plus"
+}
 // convert speech to a bag of words
 function convertSpeech(word_hash, sentence) {
     
@@ -41,6 +52,10 @@ function convertSpeech(word_hash, sentence) {
         // let unique_word = stemmer(cur_sentence[i])
         let unique_word = cur_sentence[i]
         
+        if (unique_word in SPECIAL_CHARACTER_MAP) {
+            unique_word = SPECIAL_CHARACTER_MAP[unique_word]
+        }
+
         if (!(unique_word in word_hash)){
             continue
         }
